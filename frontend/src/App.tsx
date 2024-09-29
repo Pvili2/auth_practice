7d59a12bcd5e35d74abca2d851af620d0a94419d
Padla Vili <pvilmos2003@gmail.com>
2024-09-29 22:21:37 +0200

    Verify email UI implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import FloatingShape from "./components/FloatingShape";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
+import VerifyEmail from "./pages/VerifyEmail";
 function App() {
   return (
     <div
@@ -34,6 +35,7 @@ function App() {
         <Route path="/" element={"Home"} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/verify-email" element={<VerifyEmail />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/pages/VerifyEmail.tsx
@@ -0,0 +1,109 @@
+import { FormEvent, useEffect, useRef, useState } from "react";
+//import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+import { CustomInputEvent } from "../types";
+import FormButton from "../components/FormButton";
+const VerifyEmail = () => {
+  const [code, setCode] = useState(["", "", "", "", "", ""]);
+  const inputRefs = useRef<HTMLInputElement[]>([]);
+  //const navigate = useNavigate();
+  const isLoading = false;
+  function handleChange(index: number, value: string): void {
+    const seged = [...code];
+
+    if (value.length > 1) {
+      const pastedCode = value.slice(0, 6).split("");
+      for (let i = 0; i < 6; i++) {
+        seged[i] = pastedCode[i] || "";
+      }
+      setCode(seged);
+      //focus on the last non-empty input or the first empty one
+      const lastIndex =
+        seged.length -
+        1 -
+        [...seged].reverse().findIndex((digit) => digit !== "");
+      const focusIndex = lastIndex < 5 ? lastIndex + 1 : 5;
+      inputRefs.current[focusIndex].focus();
+    } else {
+      seged[index] = value;
+      setCode(seged);
+
+      if (value && index < 5) {
+        inputRefs.current[index + 1].focus();
+      }
+    }
+  }
+
+  function handleKeyDown(
+    index: number,
+    e: React.KeyboardEvent<HTMLInputElement>
+  ): void {
+    if (e.key === "Backspace" && !code[index] && index > 0) {
+      inputRefs.current[index - 1].focus();
+      return;
+    }
+  }
+
+  const handleSubmit = (e?: FormEvent<HTMLFormElement>) => {
+    e?.preventDefault();
+    const verificationCode = code.join("");
+    alert(`Verification code submited: ${verificationCode}`);
+  };
+  //auto submit
+  useEffect(() => {
+    if (code.every((digit) => digit !== "")) {
+      handleSubmit();
+    }
+  }, [code]);
+
+  return (
+    <div
+      className="max-w-md w-full bg-gray-800 bg-opacity-50
+     backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+    >
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl p-8 w-full max-w-md"
+      >
+        <h2
+          className="text-3xl font-bold mb-6 text-center bg-gradient-to-r
+         from-green-400 to bg-emerald-500 text-transparent bg-clip-text"
+        >
+          Verify Your Email
+        </h2>
+        <p className="text-center text-gray-300 mb-6">
+          Enter the 6-digit code cent to your email address.
+        </p>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="flex justify-between">
+            {code.map((digit, index) => (
+              <input
+                key={index}
+                ref={(el: HTMLInputElement) => (inputRefs.current[index] = el)}
+                type="text"
+                maxLength={6}
+                value={digit}
+                onChange={(e: CustomInputEvent) =>
+                  handleChange(index, e.target.value)
+                }
+                onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) =>
+                  handleKeyDown(index, e)
+                }
+                className="w-12 h-12 text-center text-2xl font-bold bg-gray-700 text-white border2
+                           border-gray-600 rounded-lg focus:border-green-500 focus:outline-none"
+              />
+            ))}
+          </div>
+          <FormButton
+            content="Verify Email"
+            isLoading={isLoading && code.some((digit) => !digit)}
+          />
+        </form>
+      </motion.div>
+    </div>
+  );
+};
+
+export default VerifyEmail;
